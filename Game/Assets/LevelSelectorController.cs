c3cee9624c8a22e2e894440a7c7aac07f4924eb3
Nikoloz995 <nikoloz.astamidze@gmail.com>
2015-12-08 10:19:06 +0400

    New Logic, New Resolutions, New Assets

---
+++ Game/Assets/LevelSelectorController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectorController : MonoBehaviour {
+
+    
+	public void LoadLevel(int levelN)
+    {
+        Application.LoadLevel("Level_Scene_" + levelN.ToString());
+    }
+}
---
+++ Game/Assets/SettingsBtnsController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsBtnsController : MonoBehaviour {
+    public GameObject ExpendedSettings;
+	public void ShowInfo()
+    {
+
+
+    }
+
+    public void ToggleMusic()
+    {
+
+    }
+
+
+    public void PauseGame()
+    {
+
+    }
+
+
+    public void GoToHome()
+    {
+        Application.LoadLevel("StartScene");
+
+    }
+
+    public void GoToLevelSelector()
+    {
+        Application.LoadLevel("LevelSelector");
+    }
+
+    public void ToggleExpendedSettings()
+    {
+        ExpendedSettings.SetActive(!ExpendedSettings.activeInHierarchy);
+    }
+
+}
