df880493c9276f56c1b589f275a9f3547c36aa4f
Nikoloz995 <nikoloz.astamidze@gmail.com>
2015-12-18 03:30:11 +0400

    Added logos, Changed Reset Panel

---
+++ Game/Assets/StartSceneScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartSceneScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Text>().text = "თქვენ დააგროვეთ: " + GlobalParams.GetFullScore().ToString();
+	}
+
+    public void UpdateText()
+    {
+        GetComponent<Text>().text = "თქვენ დააგროვეთ: " + GlobalParams.GetFullScore().ToString();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
