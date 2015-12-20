9ef046ca6f37aa5534d8cb47ceaa7192b3e85936
Nikoloz995 <nikoloz.astamidze@gmail.com>
2015-12-20 21:30:49 +0400

    Added Mute

--- Game/Assets/Scr/Start_BtnController.cs
+++ Game/Assets/Scr/Start_BtnController.cs
@@ -3,9 +3,34 @@ using System.Collections;
 
 public class Start_BtnController : MonoBehaviour {
 
+    public GameObject SettingExpended;
+    public GameObject Music_OffImg;
 	public void ResetGame()
     {
         PlayerPrefs.DeleteAll();
+    }
+
+    public void ToggleSettingExpended()
+    {
+        SettingExpended.SetActive(!SettingExpended.activeInHierarchy);
+    }
+
+    public void ToggleMusic()
+    {
+        if (AudioListener.volume == 1)
+
+        {
+            if (!Music_OffImg.activeInHierarchy)
+                Music_OffImg.SetActive(true);
+            AudioListener.volume = 0;
+        }
+        else if (AudioListener.volume == 0)
+        {
+            if (Music_OffImg.activeInHierarchy)
+                Music_OffImg.SetActive(false);
+            AudioListener.volume = 1;
+
+        }
     }
 
 }
