d9796b53dee510d3087c697ee2f28ecb136f6e73
Nikoloz995 <nikoloz.astamidze@gmail.com>
2016-01-12 15:20:34 +0400

    Fixed Music Button

--- Game/Assets/Scr/Start_BtnController.cs
+++ Game/Assets/Scr/Start_BtnController.cs
@@ -5,6 +5,23 @@ public class Start_BtnController : MonoBehaviour {
 
     public GameObject SettingExpended;
     public GameObject Music_OffImg;
+
+    void Start()
+    {
+        print("Shemovdivar");
+
+        if (AudioListener.volume == 1)
+        {
+            if (Music_OffImg.activeInHierarchy)
+                Music_OffImg.SetActive(false);
+        }
+        else if (AudioListener.volume == 0)
+        {
+            if (!Music_OffImg.activeInHierarchy)
+                Music_OffImg.SetActive(true);
+
+        }
+    }
 	public void ResetGame()
     {
         PlayerPrefs.DeleteAll();
