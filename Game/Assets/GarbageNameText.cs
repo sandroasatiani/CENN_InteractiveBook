4bbfb44065b3ba74d4e0d93beecb89fd984eb7ef
Nikoloz995 <nikoloz.astamidze@gmail.com>
2016-01-12 16:23:02 +0400

    Fixed GarbageText, added Encouragment

---
+++ Game/Assets/GarbageNameText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GarbageNameText : MonoBehaviour {
+
+    public int SortLayer = 0;
+    // Use this for initialization
+    void Start()
+    {
+        Renderer renderer = this.gameObject.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.sortingOrder = SortLayer;
+                gameObject.GetComponent<Renderer>().sortingLayerName = "GarbageTxt";
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Game/Assets/Scr/CurrentLevelScore.cs
+++ Game/Assets/Scr/CurrentLevelScore.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class CurrentLevelScore : MonoBehaviour {
 
 	void Start () {
-        string text = "ქულა: " + GlobalParams.currentlevelScore + "@ სულ: " + GlobalParams.GetFullScore() ;
+        string text = "ყოჩაღ შენ წარმატებით @გაართვი თავი @დავალებას!!! @ქულა: " + GlobalParams.currentlevelScore + "@ სულ: " + GlobalParams.GetFullScore() ;
         text = text.Replace("@", System.Environment.NewLine);
         GetComponent<Text>().text = text;
 	}
